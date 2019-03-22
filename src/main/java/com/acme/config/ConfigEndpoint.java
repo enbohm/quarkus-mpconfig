0a67ac994bb64beb690d9130df5ce1cafbb686cf
enbohm <andreas@enbohms.se>
2019-03-22 07:29:10 +0100

    Updated endpoint

--- src/main/java/com/acme/config/ConfigEndpoint.java
+++ src/main/java/com/acme/config/ConfigEndpoint.java
@@ -4,11 +4,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.media.Content;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
-import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 @Path("/")
 public class ConfigEndpoint {
@@ -17,10 +12,6 @@ public class ConfigEndpoint {
   String myProp;
 
   @GET
-  @APIResponses(value = {@APIResponse(responseCode = "204", //
-      description = "A response with no content", content = @Content(mediaType = "application/json",
-          schema = @Schema(implementation = String.class)))})
-  @Operation(summary = "Say Hello World")
   @Path("/config")
   public Response greeting() {
     return Response.ok(myProp).build();
